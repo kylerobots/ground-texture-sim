2bb1b46d880b60e620e53d0b3c76ac77bb7e6005
Kyle M. Hart <kylemhart@gmail.com>
2021-07-03 14:16:48 -0400

    Implement templating to subscribe to arbitrary messages.

--- src/ground_texture_sim/DataSynchronizer.cpp
+++ src/ground_texture_sim/DataSynchronizer.cpp
@@ -8,23 +8,4 @@ namespace ground_texture_sim {
 		std::map<std::string, std::unique_ptr<google::protobuf::Message>> messages;
 		return messages;
 	}
-
-	template <typename T>
-	void DataSynchronizer::messageCallback(const T & message, const ignition::transport::MessageInfo & info) {
-		std::string topic_name = info.Topic();
-		// Only record if requested to.
-		if (capture_flags.at(topic_name)) {
-			synched_messages[topic_name].reset(std::make_unique<T>(message));
-			capture_flags[topic_name] = false;
-		}
-	}
-
-	template <typename T>
-	bool DataSynchronizer::registerTopic(const std::string & topic_name) {
-		bool success = node.Subscribe(topic_name, &DataSynchronizer::messageCallback<T>, this);
-		if (success) {
-			capture_flags[topic_name] = false;
-		}
-		return success;
-	}
 } // namespace ground_texture_sim
--- test/test_data_synchronizer.cpp
+++ test/test_data_synchronizer.cpp
@@ -11,3 +11,15 @@ TEST(DataSynchronizer, Empty) {
 	auto results = synchronizer.getMessages();
 	EXPECT_EQ(results.size(), 0);
 }
+
+/**
+ * @brief Tests that the synchronizer can subscribe to multiple topics of different types.
+ * 
+ */
+TEST(DataSynchronizer, Subscribe) {
+	ground_texture_sim::DataSynchronizer synchronizer;
+	bool success = true;
+	success &= synchronizer.registerTopic<ignition::msgs::Image>("/camera");
+	success &= synchronizer.registerTopic<ignition::msgs::Pose>("/pose");
+	EXPECT_TRUE(success);
+}
