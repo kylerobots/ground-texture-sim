42bc98131e2384e7d267b04e26c43e7aa2b65f49
Kyle M. Hart <kylemhart@gmail.com>
2021-07-04 08:23:53 -0400

    Fix wait until recorded logic.

--- src/ground_texture_sim/DataSynchronizer.cpp
+++ src/ground_texture_sim/DataSynchronizer.cpp
@@ -9,11 +9,13 @@ namespace ground_texture_sim {
 		for (auto it = capture_flags.begin(); it != capture_flags.end(); ++it) {
 			it->second = true;
 		}
-		// Now just wait until all messages have been updated.
-		bool all_captured = true;
-		while (!all_captured) {
+		// Now just wait until all messages have been updated. The flag gets set to false after message capture, so any
+		// true value means a message is pending.
+		bool still_pending = true;
+		while (!still_pending) {
+			still_pending = false;
 			for (auto & capture_flag : capture_flags) {
-				all_captured &= capture_flag.second;
+				still_pending |= capture_flag.second;
 			}
 		}
 		// Once done, return the map, which will copy it out.
