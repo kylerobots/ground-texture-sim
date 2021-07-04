7f03ad58ef05b764b2f3628bb2004f6f5c0ee664
Kyle M. Hart <kylemhart@gmail.com>
2021-07-04 11:35:22 -0400

    Add check to ensure messages are correct.

--- src/ground_texture_sim/TrajectoryFollower.cpp
+++ src/ground_texture_sim/TrajectoryFollower.cpp
@@ -14,12 +14,34 @@ namespace ground_texture_sim {
 			if (!success) {
 				return false;
 			}
-			// Now get the most up to date messages.
-			auto messages = data_synchronizer.getMessages();
-			// Cast each message back into the expected format.
-			auto camera_info_msg = dynamic_cast<ignition::msgs::CameraInfo *>(messages["/camera_info"].get());
-			auto image_msg = dynamic_cast<ignition::msgs::Image *>(messages["/camera"].get());
-			auto pose_msg = dynamic_cast<ignition::msgs::Pose_V *>(messages["/world/ground_texture/dynamic_pose/info"].get());
+			ignition::msgs::CameraInfo * camera_info_msg;
+			ignition::msgs::Image * image_msg;
+			ignition::msgs::Pose_V * pose_msg;
+			// Now get the most up to date messages. There might be a bit of a lag in updating the values, so check
+			// the pose to see if it matches approximately what we would expect.
+			bool pose_updated = false;
+			while (!pose_updated) {
+				auto messages = data_synchronizer.getMessages();
+				// Cast each message back into the expected format.
+				camera_info_msg = dynamic_cast<ignition::msgs::CameraInfo *>(messages["/camera_info"].get());
+				image_msg = dynamic_cast<ignition::msgs::Image *>(messages["/camera"].get());
+				pose_msg = dynamic_cast<ignition::msgs::Pose_V *>(messages["/world/ground_texture/dynamic_pose/info"].get());
+				// Compare values to verify updated pose. Account for floating point accuracy.
+				ignition::msgs::Pose extracted_pose;
+				for (int i = 0; i < pose_msg->pose_size(); ++i) {
+					if (pose_msg->pose(i).name() == "camera") {
+						extracted_pose = pose_msg->pose(i);
+					}
+				}
+				// We want all pose values to be updated, so flip to false if any aren't within eps.
+				pose_updated = true;
+				pose_updated &= abs(extracted_pose.position().x() - pose.x) <= 1e-8;
+				pose_updated &= abs(extracted_pose.position().y() - pose.y) <= 1e-8;
+				// We have to pull out the quaternion values.
+				ignition::math::Quaternion quaternion(extracted_pose.orientation().w(), extracted_pose.orientation().x(), extracted_pose.orientation().y(), extracted_pose.orientation().z());
+				pose_updated &= abs(quaternion.Yaw() - pose.yaw) <= 1e-8;
+			}
+
 			// Write them to file.
 			data_writer.registerCameraInfo(*camera_info_msg);
 			data_writer.registerPose(*pose_msg);
