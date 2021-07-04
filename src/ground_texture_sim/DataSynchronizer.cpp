71b1656bf457d29fb2f39aa01d1efbd8b6abc84e
Kyle M. Hart <kylemhart@gmail.com>
2021-07-04 10:49:05 -0400

    Fix logic error in waiting for updated messages.

--- src/ground_texture_sim/DataSynchronizer.cpp
+++ src/ground_texture_sim/DataSynchronizer.cpp
@@ -12,7 +12,7 @@ namespace ground_texture_sim {
 		// Now just wait until all messages have been updated. The flag gets set to false after message capture, so any
 		// true value means a message is pending.
 		bool still_pending = true;
-		while (!still_pending) {
+		while (still_pending) {
 			still_pending = false;
 			for (auto & capture_flag : capture_flags) {
 				still_pending |= capture_flag.second;
