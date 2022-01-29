5a098700f796a5ffecbf2ea984859a9cc7ae2662
Kyle M. Hart <kylemhart@gmail.com>
2022-01-29 10:52:37 -0500

    Add reading of JSON config file

--- data_generation.py
+++ data_generation.py
@@ -8,7 +8,32 @@ specified directory.
 
 SPDX-License-Identifier: GPL-3.0-or-later
 """
-from typing import List
+import json
+from typing import Dict, List
+
+
+def read_config(filename: str) -> Dict:
+    """!
+    Reads in the configuration JSON and verifies required components.
+
+    This reads the JSON file specified in filename. It also checks that, at a
+    minimum, the output directory and trajectory file have been specified. If
+    they are not, an Exception is raised.
+
+    @param filename The JSON file to read. May be absolute or relative path.
+    @return The JSON, loaded as a Dict.
+    @exception FileNotFoundError Raised if the file provided in filename does
+    not exist.
+    @exception KeyError Raised if the required entries are not present in the
+    JSON.
+    @exception JSONDecoderError Raised if the file is not in JSON format.
+    """
+    with open(file=filename, mode='r', encoding='utf8') as file:
+        parameters = json.load(fp=file)
+    # Check for required options
+    if 'trajectory' not in parameters.keys() or 'output' not in parameters.keys():
+        raise KeyError('Required values "output" and "trajectory" not in JSON')
+    return parameters
 
 
 def read_poses(filename: str) -> List[List[float]]:
@@ -23,10 +48,10 @@ def read_poses(filename: str) -> List[List[float]]:
     @param filename The file to read from. May be absolute or relative path.
     @return A list of poses, where each item in the list is a list of the form
     [x, y, theta].
-    @exception RuntimeError Raised if the pose format does not follow the correct
-    structure.
-    @exception FileNotFoundError Raised if the file provided in filename does not
-    exist.
+    @exception FileNotFoundError Raised if the file provided in filename does
+    not exist.
+    @exception RuntimeError Raised if the pose format does not follow the
+    correct structure.
     """
     result = []
     with open(file=filename, mode='r', encoding='utf8') as file:
--- test_data_generation.py
+++ test_data_generation.py
@@ -3,9 +3,71 @@ This module provides the unit tests for the data_generation module.
 
 SPDX-License-Identifier: GPL-3.0-or-later
 """
+from json import JSONDecodeError
 import unittest
 from unittest.mock import mock_open, patch
-from data_generation import read_poses
+from data_generation import read_config, read_poses
+
+
+class TestReadConfig(unittest.TestCase):
+    """!
+    This class tests the read_config function.
+    """
+
+    def test_missing_required_entries(self) -> None:
+        """!
+        Test the function raises an Exception if required entries are not
+        present.
+
+        At a minimum, the JSON should specify the output directory and the
+        trajectory file.
+
+        @return None
+        """
+        input_string = '{\n"trajectory": "trajectory.txt",\n"gpu": true\n}'
+        with patch(target='builtins.open', new=mock_open(read_data=input_string)):
+            self.assertRaises(KeyError, read_config, 'trajectory.txt',)
+
+    def test_nonexistent_file(self) -> None:
+        """!
+        Test the function raises an Exception if the specified file does not
+        exist.
+
+        Normally, we mock the file open. However, in this case, we can just
+        pass a file that doesn't actually exist and expect the error.
+
+        @return None
+        """
+        self.assertRaises(FileNotFoundError, read_config,
+                          'non_a_real_file.json')
+
+    def test_not_json(self) -> None:
+        """!
+        Test the function raises an Exception if the specified file is not in
+        JSON format.
+
+        @return None
+        """
+        input_string = 'not a json\n'
+        with patch(target='builtins.open', new=mock_open(read_data=input_string)):
+            self.assertRaises(JSONDecodeError, read_config, 'trajectory.txt')
+
+    def test_successful(self) -> None:
+        """!
+        Test the function works as expected if the specified file is correct.
+
+        @return None
+        """
+        input_string = '{\n"trajectory": "trajectory.txt",\n"output": "output/",\n"gpu": true\n}'
+        expected_result = {
+            'output': 'output/',
+            'trajectory': 'trajectory.txt',
+            'gpu': True
+        }
+        with patch(target='builtins.open', new=mock_open(read_data=input_string)):
+            result = read_config('config.json')
+            self.assertDictEqual(d1=result, d2=expected_result,
+                                 msg='Unable to read JSON into Dict')
 
 
 class TestReadPoses(unittest.TestCase):
@@ -17,6 +79,7 @@ class TestReadPoses(unittest.TestCase):
         """!
         This method verifies that the function returns an empty list if the file
         is empty.
+
         @return None
         """
         input_string = '\n\n'
@@ -31,6 +94,7 @@ class TestReadPoses(unittest.TestCase):
 
         Normally, we mock the file open. However, in this case, we can just
         pass a file that doesn't actually exist and expect the error.
+
         @return None
         """
         self.assertRaises(FileNotFoundError, read_poses, 'non_a_real_file.txt')
@@ -39,6 +103,7 @@ class TestReadPoses(unittest.TestCase):
         """!
         This method verifies that the function throws an exception if any of the
         poses do not define all three numbers (X, Y, and Yaw)
+
         @return None
         """
         input_string = '0.0,0.0,0.0\n0.0,0.0,\n'
@@ -52,6 +117,7 @@ class TestReadPoses(unittest.TestCase):
         """!
         This method verifies that the function throws an exception if any of
         the poses are not composed of numbers.
+
         @return None
         """
         input_string = '0.0,0.0,0.0\n0.0,0.0,the\n'
@@ -62,6 +128,7 @@ class TestReadPoses(unittest.TestCase):
         """!
         This method verifies that reading poses from files works even without
         whitespace between the numbers.
+
         @return None
         """
         input_string = '0.0,0.0,0.0\n1.0,2.0,3.0'
@@ -75,6 +142,7 @@ class TestReadPoses(unittest.TestCase):
         """!
         This method verifies that reading poses from files can appropriately
         strip whitespace when reading.
+
         @return None
         """
         input_string = '0.0, 0.0, 0.0\n1.0, 2.0, 3.0\n\n'
