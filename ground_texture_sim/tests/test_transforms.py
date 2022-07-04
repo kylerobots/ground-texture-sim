b7c66a930fe388bf775e67be3da8462ea99747c6
Kyle M. Hart <kylemhart@gmail.com>
2022-07-04 09:07:41 -0400

    Create class to manage transforming data between frames

--- ground_texture_sim/tests/test_transforms.py
+++ ground_texture_sim/tests/test_transforms.py
@@ -35,3 +35,126 @@ class TestCreateTransformMatrix(unittest.TestCase):
         result = transforms.create_transform_matrix(
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
         self.assertTrue(numpy.allclose(expected_result, result))
+
+
+class TestTransformer(unittest.TestCase):
+    """!
+    @brief Test the Transformer class works as expected.
+    """
+
+    def test_camera_matrix_setter(self) -> None:
+        """!
+        @brief Test that the getter and setter for camera intrinsic matrix works.
+        @return None
+        """
+        camera_matrix = numpy.array([
+            [200.0, 0.0, 500.0],
+            [0.0, 200.0, 500.0],
+            [0.0, 0.0, 1.0]
+        ])
+        transformer = transforms.Transformer(numpy.identity(4), camera_matrix)
+        self.assertTrue(numpy.allclose(camera_matrix, transformer.camera_intrinsic_matrix),
+                        msg='Constructor does not set intrinsic matrix.')
+        transformer.camera_intrinsic_matrix = numpy.identity(3)
+        self.assertTrue(numpy.allclose(
+            numpy.identity(3), transformer.camera_intrinsic_matrix),
+            msg='Setter does not set intrinsic matrix.')
+
+    def test_camera_matrix_reject_size(self) -> None:
+        """!
+        @brief Test that the setter rejects if the wrong size intrinsic matrix is provided.
+        @return None
+        """
+        with self.assertRaises(ValueError,
+                               msg='Transformer does not raise exception on bad intrinsic matrix.'):
+            transforms.Transformer(numpy.identity(4), numpy.identity(2))
+        with self.assertRaises(ValueError,
+                               msg='Transformer does not raise exception on bad intrinsic.'):
+            transformer = transforms.Transformer(
+                numpy.identity(4), numpy.identity(3))
+            transformer.camera_intrinsic_matrix = numpy.identity(2)
+
+    def test_camera_pose_setter(self) -> None:
+        """!
+        @brief Test that the getter and setter for camera pose works.
+        @return None
+        """
+        pose = numpy.array([
+            [0.0, 1.0, 0.0, 1.0],
+            [1.0, 0.0, 0.0, 2.0],
+            [0.0, 0.0, 1.0, 3.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        transformer = transforms.Transformer(pose, numpy.identity(3))
+        self.assertTrue(numpy.allclose(pose, transformer.camera_pose),
+                        msg='Constructor does not set camera pose.')
+        transformer.camera_pose = numpy.identity(4)
+        self.assertTrue(numpy.allclose(
+            numpy.identity(4), transformer.camera_pose), msg='Setter does not set camera pose.')
+
+    def test_camera_pose_reject_size(self) -> None:
+        """!
+        @brief Test that the setter rejects if the wrong size pose is provided.
+        @return None
+        """
+        with self.assertRaises(ValueError, msg='Transformer does not raise exception on bad pose.'):
+            transforms.Transformer(numpy.identity(2), numpy.identity(3))
+        with self.assertRaises(ValueError, msg='Transformer does not raise exception on bad pose.'):
+            transformer = transforms.Transformer(
+                numpy.identity(4), numpy.identity(3))
+            transformer.camera_pose = numpy.identity(2)
+
+    def test_transform_camera_to_world_full(self) -> None:
+        """!
+        @brief Verify that the transform to world function works for some arbitrary values.
+        @return None
+        """
+        robot_pose = numpy.array([
+            [-1.0, 0.0, 0.0, 1.0],
+            [0.0, -1.0, 0.0, 2.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        camera_pose = numpy.array([
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [-1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        expected_result = numpy.array([
+            [0.0, 0.0, -1.0, 1.0],
+            [0.0, -1.0, 0.0, 2.0],
+            [-1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        transformer = transforms.Transformer(camera_pose, numpy.identity(3))
+        result = transformer.transform_camera_to_world(robot_pose)
+        self.assertTrue(numpy.allclose(result, expected_result),
+                        msg='Transform math is not correct.')
+
+    def test_transform_camera_to_world_identity(self) -> None:
+        """!
+        @brief Verify that the transform to world function works when different elements are
+        identity.
+        @return None
+        """
+        other_matrix = numpy.array([
+            [0.0, 1.0, 0.0, 1.0],
+            [1.0, 0.0, 0.0, 2.0],
+            [0.0, 0.0, 1.0, 3.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+        transformer = transforms.Transformer(
+            numpy.identity(4), numpy.identity(3))
+        # Test when both are identity
+        result = transformer.transform_camera_to_world(numpy.identity(4))
+        self.assertTrue(numpy.allclose(result, numpy.identity(4)),
+                        msg='Transform incorrect for identity.')
+        # Test when only one is
+        result = transformer.transform_camera_to_world(other_matrix)
+        self.assertTrue(numpy.allclose(result, other_matrix),
+                        msg='Transform incorrect for identity.')
+        transformer.camera_pose = other_matrix
+        result = transformer.transform_camera_to_world(numpy.identity(4))
+        self.assertTrue(numpy.allclose(result, other_matrix),
+                        msg='Transform incorrect for identity.')
--- ground_texture_sim/transforms.py
+++ ground_texture_sim/transforms.py
@@ -42,3 +42,77 @@ def create_transform_matrix(x: float, y: float, z: float, roll: float, pitch: fl
     ])
     pose[0:3, 0:3] = rotation_roll @ rotation_pitch @ rotation_yaw
     return pose
+
+
+class Transformer:
+    """!
+    @brief A class to convert poses and points from one frame to another.
+    """
+
+    def __init__(self, camera_pose: numpy.ndarray, camera_intrinsic_matrix: numpy.ndarray) -> None:
+        """!
+        @brief Construct the class with the pose of the camera relative to the robot and camera
+        intrinsic matrix.
+        @param camera_pose A 4x4 Numpy array that represents the homogenous pose of the camera as
+        measured from the robot's frame of reference.
+        @param camera_intrinsic_matrix The 3x3 Numpy array that holds the camera's intrinsic matrix.
+        """
+        ## The camera's pose, as measured from the robot's frame of reference.
+        self.camera_pose = camera_pose
+        ## The camera intrinsic matrix, generally set in Blender.
+        self.camera_intrinsic_matrix = camera_intrinsic_matrix
+
+    @property
+    def camera_intrinsic_matrix(self) -> numpy.ndarray:
+        """!
+        @brief Get the 3x3 camera intrinsic matrix.
+        @return A 3x3 Numpy array representing the matrix.
+        """
+        return self._camera_intrinsic_matrix
+
+    @camera_intrinsic_matrix.setter
+    def camera_intrinsic_matrix(self, camera_intrinsic_matrix: numpy.ndarray) -> None:
+        """!
+        @brief Set the camera intrinsic matrix.
+        @param camera_intrinsic_matrix A 3x3 Numpy array representing the matrix.
+        @return None
+        @exception ValueError if the provided matrix is not 3x3.
+        """
+        if camera_intrinsic_matrix.shape != (3, 3):
+            raise ValueError(
+                F'Provided intrinsic matrix should be shape (3, 3), '
+                F'not {camera_intrinsic_matrix.shape}.')
+        ## The camera intrinsic matrix, generally set in Blender.
+        self._camera_intrinsic_matrix = camera_intrinsic_matrix
+
+    @property
+    def camera_pose(self) -> numpy.ndarray:
+        """!
+        @brief Get the 4x4 homogenous pose of the camera as measured from the robot.
+        @return A 4x4 Numpy array representing the pose.
+        """
+        return self._camera_pose
+
+    @camera_pose.setter
+    def camera_pose(self, camera_pose: numpy.ndarray) -> None:
+        """!
+        @brief Set the pose of the camera as measured from the robot's frame of reference.
+        @param camera_pose A 4x4 Numpy array containing a homogenous pose.
+        @return None
+        @exception ValueError if the provided matrix is not 4x4.
+        """
+        if camera_pose.shape != (4, 4):
+            raise ValueError(
+                F'Provided intrinsic matrix should be shape (4, 4), not {camera_pose.shape}.')
+        ## The camera's pose, as measured from the robot's frame of reference.
+        self._camera_pose = camera_pose
+
+    def transform_camera_to_world(self, robot_pose: numpy.ndarray) -> numpy.ndarray:
+        """!
+        @brief Given a robot's pose in the world, determine the pose of the camera in the world.
+        @param robot_pose A 4x4 Numpy array containing the homogenous representation of the robot as
+        measured from the world frame.
+        @return A 4x4 Numpy array representing the pose of the camera as measured from the world
+        frame.
+        """
+        return robot_pose @ self.camera_pose
